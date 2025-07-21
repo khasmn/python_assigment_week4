b2d9cbcfc170a4342fe532331832b9c65c41d19f
Thammasorn Srimanee <kha@Thammasorns-MacBook-Air.local>
2025-07-21 14:58:45 +0700

    finsih EX01

---
+++ ex01.py
@@ -0,0 +1,6 @@
+from calculator import add, subtract, multiply, divide
+
+print("Addition:", add(2, 4))
+print("Subtraction:", subtract(2, 4))
+print("Multiplication:", multiply(2, 4))
+print("Division:", divide(2, 4))
