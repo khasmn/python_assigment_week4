d6e2646f8e5a9e13c01903d7bfea413fcc7d2c26
Thammasorn Srimanee <kha@Thammasorns-MacBook-Air.local>
2025-07-21 15:07:34 +0700

    EX03 finish

---
+++ ex02.py
@@ -0,0 +1,6 @@
+from lambda_calator import add, subtract, multiply, divide
+
+print("Addition:", add(2, 4))
+print("Subtraction:", subtract(2, 4))
+print("Multiplication:", multiply(2, 4))
+print("Division:", divide(2, 4))
---
+++ lambda_calator.py
@@ -0,0 +1,5 @@
+add = lambda a, b: a + b
+subtract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b if b != 0 else "Error"
+
